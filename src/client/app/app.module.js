ad7dce88ba8d629c0f65592c253df2ca471856a4
quangdang1995 <danghuuthanh95@gmail.com>
2016-07-04 22:24:56 +0700

    Revert "Final Project"
    This reverts commit 59ff2822d7247cffa3e5c582ea298bcdbf5f54ff.

--- src/client/app/app.module.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular.module('app', [
-        'app.core',
-        'app.landing', 'app.profile', 'app.authentication',
-        'app.widgets', 'app.layout'
-    ]);
-})();
